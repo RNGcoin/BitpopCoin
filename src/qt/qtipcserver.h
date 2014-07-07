7043efc71b5340d32ea2ebafa0c1c2837deae486
bitpop <bitpop>
2014-07-08 02:31:44 +0500

    initial commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define BitpopCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "BitpopCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
